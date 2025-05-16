c7418a44a2d30b2599c2937012cd9696b7dc048a
glaydson-alves <glaydson.alves36@gmail.com>
2025-05-16 12:41:39 -0300

    refactor: adiciona types

--- utils/types.ts
+++ utils/types.ts
@@ -1,5 +1,16 @@
+import { UserRole } from "src/users/entities/user.entity";
+
 export type UserDatails = {
     email: string,
     displayName: string,
     url_avatar: string,
 }
+
+export type AuthJwtPayload = {
+    sub: string;
+};
+
+export type CurrentUser = {
+    id: string;
+    role: UserRole;
+};
