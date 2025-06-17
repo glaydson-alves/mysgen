c9604691719830c8c16f94f90dce3c7c1ea35d25
glaydson-alves <glaydson.alves36@gmail.com>
2025-06-17 12:27:19 -0300

    refactor: refatora update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+    @IsOptional()
+    displayName?: string;
+
+    @IsOptional()
+    url_avatar?: string
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadGatewayException, BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -14,23 +14,24 @@ export class UsersService {
     private userRepository: Repository<User>,
   ) {}
 
-  async findOne(id: number){
-    const user = await this.userRepository.findOne({where: {id}})
-    if (!user) {
-      throw new BadRequestException(`User with ID ${id} not found`)
-    }
-    return new ResponseDto('User updated successfully', user);
-  }
-
-
   async update(id: number, updateUserDto: UpdateUserDto){
-    const user = await this.userRepository.findOne({where: {id}})
-    if (!user) {
-      throw new BadRequestException(`User with ID ${id} not found`)
+    if(!updateUserDto.url_avatar){
+      delete updateUserDto.url_avatar
     }
-    Object.assign(user, updateUserDto);
-    await this.userRepository.save(user);
-    return new ResponseDto('User updated successfully', user);
+    if(!updateUserDto.displayName){
+      delete updateUserDto.displayName
+    }
+    try {
+      const result = await this.userRepository.update(id, updateUserDto);
+      if (result.affected === 0) {
+        throw new BadRequestException(`User with id${id} not found`)
+      }
+      const user = await this.userRepository.findOne({where: {id}});
+
+      return new ResponseDto('User updated successfully', user);
+    } catch (error) {
+      throw new BadGatewayException('Failed to update user')
+    }
   }
 
   
