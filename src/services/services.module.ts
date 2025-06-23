7cc2309bb018a5fb67cde64dbcd0ab70174d8a23
glaydson-alves <glaydson.alves36@gmail.com>
2025-06-23 10:13:21 -0300

    refactor: adiciona enterprise em service module

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -3,12 +3,13 @@ import { ServicesService } from './services.service';
 import { ServicesController } from './services.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Service } from './entities/service.entity';
+import { Enterprise } from 'src/enterprise/entities/enterprise.entity';
 
 @Module({
   controllers: [ServicesController],
   providers: [ServicesService],
   imports: [
-    TypeOrmModule.forFeature([Service]),
+    TypeOrmModule.forFeature([Service, Enterprise]),
   ],
 })
 export class ServicesModule {}
