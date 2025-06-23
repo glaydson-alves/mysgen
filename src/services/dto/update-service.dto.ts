09096f9ce5d97d64881eeb08c53d9721e5b429de
glaydson-alves <glaydson.alves36@gmail.com>
2025-06-23 15:35:07 -0300

    refactor: define UpdateServiceDto

--- src/services/dto/update-service.dto.ts
+++ src/services/dto/update-service.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateServiceDto } from './create-service.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
+export class UpdateServiceDto extends PartialType(CreateServiceDto) {
+
+    @IsOptional()
+    name?: string;
+
+    @IsOptional()
+    price?: number;
+    
+    @IsOptional()
+    description?: string;
+
+    @IsOptional()
+    duration_minutes?: number;
+
+    @IsOptional()
+    image_url?: string;
+}
