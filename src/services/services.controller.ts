e743c45220a88c4b921e47214c6bf57a3b5018ef
glaydson-alves <glaydson.alves36@gmail.com>
2025-06-23 15:36:28 -0300

    feat: implementa crud de services

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -1,33 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { ServicesService } from './services.service';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
+import { JwtAuthGuard } from 'src/auth/utils/guards/jwt.auth.guard';
+import { RolesGuard } from 'src/auth/utils/guards/roles.guard';
+import { Roles } from 'src/auth/utils/decorators/roles.decorator';
+import { UserRole } from 'src/users/entities/user.entity';
 
 @Controller('services')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ServicesController {
   constructor(private readonly servicesService: ServicesService) {}
 
-  @Post()
+  @Post('create')
+  @Roles(UserRole.ADMIN)
   create(@Body() createServiceDto: CreateServiceDto) {
-    return this.servicesService.create(createServiceDto);
+    const { enterprise_id } = createServiceDto;
+    return this.servicesService.create(enterprise_id,createServiceDto);
   }
 
-  @Get()
-  findAll() {
-    return this.servicesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.servicesService.findOne(+id);
+  @Get('all')
+  //aqui é uma rota publica
+  findAll(@Query('enterprise_id') enterpriseId: number) {
+    return this.servicesService.findAll(+enterpriseId);
   }
 
   @Patch(':id')
+  @Roles(UserRole.ADMIN)
   update(@Param('id') id: string, @Body() updateServiceDto: UpdateServiceDto) {
     return this.servicesService.update(+id, updateServiceDto);
   }
 
   @Delete(':id')
+  @Roles(UserRole.ADMIN)
   remove(@Param('id') id: string) {
     return this.servicesService.remove(+id);
   }
--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,46 +1,112 @@
-import { BadGatewayException, BadRequestException, Injectable } from '@nestjs/common';
+import { BadGatewayException, BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Service } from './entities/service.entity';
 import { Repository } from 'typeorm';
-import { User } from 'src/users/entities/user.entity';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import { Enterprise } from 'src/enterprise/entities/enterprise.entity';
+import { ResponseDto } from 'src/common/dto/response/response';
 
 @Injectable()
 export class ServicesService {
   constructor(
     @InjectRepository(Service)
     private readonly servicesRepository: Repository<Service>,
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
     @InjectRepository(Enterprise)
-    private readonly enterpirseRespository: Repository<Enterprise>
+    private readonly enterpriseRepository: Repository<Enterprise>
   ){}
   async create(enterprise_id: number, createServiceDto: CreateServiceDto) {
-    const existingEnterprise = await this.enterpirseRespository.find({ where: { id: enterprise_id } })
+    const existingEnterprise = await this.enterpriseRepository.findOne({ where: { id: enterprise_id } });
     if (!existingEnterprise) {
       throw new BadRequestException(`Enterprise with ID ${enterprise_id} not found`);
     }
-    // const newService = this.servicesRepository.create({
-    //   ...CreateServiceDto,
-    //   enterprise,
-    // })
+  
+    const { enterprise_id: _, ...rest } = createServiceDto;
+    const newService = this.servicesRepository.create({
+      ...rest,
+      enterprise: { id: enterprise_id },
+    });
+  
+    await this.servicesRepository.save(newService);
+    return new ResponseDto('Service created successfully', newService);
   }
 
-  findAll() {
-    return `This action returns all services`;
+  async findAll(enterprise_id: number) {
+    const enterprise = await this.enterpriseRepository.findOne({
+      where: { id: enterprise_id },
+    });
+  
+    if (!enterprise) {
+      throw new NotFoundException('Enterprise not found');
+    }
+
+    const allServices = await this.servicesRepository.find({
+      where: {
+        enterprise: {id: enterprise_id}
+      },
+      order:{
+        created_at: 'DESC'
+      }
+    })
+    return new ResponseDto('Services loaded', allServices);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} service`;
+  async update(id: number, updateServiceDto: UpdateServiceDto) {
+    if (!updateServiceDto || Object.keys(updateServiceDto).length === 0) {
+      throw new BadRequestException('Update data cannot be empty');
+    }
+    if (!updateServiceDto.name) {
+      delete updateServiceDto.name;
+    }
+    if (!updateServiceDto.price) {
+      delete updateServiceDto.price;
+    }
+    if (!updateServiceDto.description) {
+      delete updateServiceDto.description;
+    }
+    if (!updateServiceDto.duration_minutes) {
+      delete updateServiceDto.duration_minutes;
+    }
+    if (!updateServiceDto.image_url) {
+      delete updateServiceDto.image_url;
+    }
+    try {
+      const result = await this.servicesRepository.update(id, updateServiceDto);
+      if (result.affected === 0) {
+        throw new BadRequestException(`Service with ID ${id} not found`);
+      }
+      const updatedService = await this.servicesRepository.findOne({ where: { id } });
+      const response = {
+        name: updatedService?.name,
+        price: updatedService?.price,
+        description: updatedService?.description,
+        duration_minutes: updatedService?.duration_minutes,
+        image_url: updatedService?.image_url
+      }
+      return new ResponseDto('Service updated successfully', response)
+
+    } catch (error) {
+      throw new BadGatewayException('Failed to update service');
+    }
   }
 
-  update(id: number, updateServiceDto: UpdateServiceDto) {
-    return `This action updates a #${id} service`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} service`;
+  async remove(serviceId: number) {
+    const service = await this.servicesRepository.findOne({
+      where: { id:serviceId },
+      relations: ['enterprise', 'enterprise.user'],
+    });
+    if (!service) {
+      throw new NotFoundException(`Service with ID ${serviceId} not found`)
+    }
+    const enterprise = await this.enterpriseRepository.findOne({
+      where: { id: service.enterprise.id },
+      relations: ['user']
+    })
+    if (!enterprise || enterprise.user.role !== UserRole.ADMIN) {
+      throw new ForbiddenException('You do not have permission to delete this service')
+    };
+    await this.servicesRepository.remove(service)
+    return new ResponseDto('Service removed successfully')
   }
 }
