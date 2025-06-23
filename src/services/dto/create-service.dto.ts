b4966185059265c0af6628f028c0f808538fa5b6
glaydson-alves <glaydson.alves36@gmail.com>
2025-06-23 10:15:28 -0300

    refactor: cria create serviceDto

--- src/services/dto/create-service.dto.ts
+++ src/services/dto/create-service.dto.ts
@@ -1,1 +1,8 @@
-export class CreateServiceDto {}
+export class CreateServiceDto {
+    enterprise_id: number;
+    name: string;
+    price: number;
+    description: string;
+    duration_minutes: number;
+    image_url: string;
+}
