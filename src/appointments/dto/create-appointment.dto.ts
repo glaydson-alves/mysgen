d5049e58eb7125ae265104d1198717dbd12b2f78
glaydson-alves <glaydson.alves36@gmail.com>
2025-06-24 09:18:43 -0300

    feat: adiciona dto de createAppointment

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,15 @@
-export class CreateAppointmentDto {}
+import { IsEnum, IsNotEmpty, IsNumber } from "class-validator";
+import { AppointmentStatus } from "../entities/appointment.entity";
+
+export class CreateAppointmentDto {
+
+    @IsNumber()
+    @IsNotEmpty()
+    service_id: number;
+
+    @IsNotEmpty()
+    scheduled_day: Date;
+
+    @IsEnum(AppointmentStatus)
+    status?: AppointmentStatus
+}
